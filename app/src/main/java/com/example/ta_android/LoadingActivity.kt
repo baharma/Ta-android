22f8ed0eef117513c25d6d98bf199d849d6cd85f
baharma <bagusaditya1899@gmail.com>
2023-11-25 03:24:55 +0800

    update desain login

---
+++ app/src/main/java/com/example/ta_android/LoadingActivity.kt
@@ -0,0 +1,27 @@
+package com.example.ta_android
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+import com.example.ta_android.databinding.ActivityLoadingBinding
+
+class LoadingActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoadingBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoadingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
